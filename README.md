950a44eaba42f7545d0d2cf99355293a97489956
ozkar <oscarmg99@gmail.com>
2022-02-05 00:32:53 -0800

    implements winput functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # footloose
 
-footloose is a small utiliy that transforms midi CC messages into keystrokes.
+footloose is a (windows only) small utiliy that transforms midi CC messages into windows inputs (keystrokes, mouse clicks, and mouse movements, etc...),
 
-footloose does not care for program changes or the values of the control changes messages.
+it is configured by editing its source code, its pre-configured with a simple `ctrl+c` and `ctrl+v` example, for CC `53` and `52` respectively.
 
-its windows only but it can (probably?) be extended to linux/macosx if the core-midi library supports them.
+footloose does not care for program changes or the values of the control changes messages, although this could easily be extended for that use-case.
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,55 @@
 extern crate midi_control;
 extern crate midir;
+extern crate winput;
 
+use std::collections::HashMap;
 use std::sync::mpsc::channel;
 use std::thread;
 use std::time;
 
 use midi_control::MidiMessage;
-
-// The name of your MIDI device.
-const MIDI_DEVICE_NAME: &str = "TSMIDI2.0";
+use winput::{Action, Input, Vk};
 
 fn main() {
+
+    /* Start Configuration */
+
+    // The name of your MIDI device.
+    let midi_device_name: &str = "TSMIDI2.0";
+
+    // The configuration for the CC messages.
+    let configuration = HashMap::from([
+        (
+            53_u8, // ctrl+c
+            vec![
+                Input::from_vk(Vk::Control, Action::Press),
+                Input::from_vk(Vk::C, Action::Press),
+                Input::from_vk(Vk::Control, Action::Release),
+                Input::from_vk(Vk::C, Action::Release),
+            ],
+        ),
+        (
+            52_u8, // ctrl+v
+            vec![
+                Input::from_vk(Vk::Control, Action::Press),
+                Input::from_vk(Vk::V, Action::Press),
+                Input::from_vk(Vk::Control, Action::Release),
+                Input::from_vk(Vk::V, Action::Release),
+            ],
+        ),
+    ]);
+
+    /* End Configuration */
 
     // create midi input.
     let midi_input = midir::MidiInput::new("MIDITest").unwrap();
 
     // find our device.
-    let device_port = find_port(&midi_input, MIDI_DEVICE_NAME);
+    let device_port = find_port(&midi_input, midi_device_name);
     if device_port.is_none() {
         println!("Input device not found!");
         return;
     }
-
 
     // create channel for sending/receiving from inside the loop.
     let (sender, receiver) = channel::<MidiMessage>();
@@ -31,31 +59,32 @@ fn main() {
     let device_port = device_port.unwrap();
     let _connect_in = midi_input.connect(
         &device_port,
-        MIDI_DEVICE_NAME,
+        midi_device_name,
         move |_, data, sender| {
             let msg = MidiMessage::from(data);
             if let Err(e) = sender.send(msg) {
                 println!("Failed to send message to channel: {:?}", e);
-                return;
             }
         },
         sender,
     );
 
-
     // set a loop to handle received messages.
     let hundred_millis = time::Duration::from_millis(100);
     loop {
-          if let Ok(msg) = receiver.recv() {
+        if let Ok(msg) = receiver.recv() {
             match msg {
-                MidiMessage::ControlChange(_, control_event) => println!("CC {:?} received", control_event.control), // handle accordingly.
+                MidiMessage::ControlChange(_, control_event) => {
+                    execute_keystrokes(&configuration, &control_event.control)
+                } // handle accordingly.
                 _ => println!("Unsupported MIDI message: {:?}", msg),
             };
-          }
+        }
         thread::sleep(hundred_millis);
     }
 }
 
+// find_port will find the correct port associated to the midi device.
 fn find_port<T>(midi_io: &T, device_port_name: &str) -> Option<T::Port>
 where
     T: midir::MidiIO,
@@ -70,4 +99,15 @@ where
         }
     }
     device_port
+}
+
+// execute keystrokes searches for the corresponding CC in the configuration
+// and if it exists, executes the inputs.
+fn execute_keystrokes(configuration: &HashMap<u8, std::vec::Vec<winput::Input>>, cc: &u8) {
+    println!("CC {:?} received", cc);
+
+    if let Some(inputs) = configuration.get(cc) {
+        println!("Executing keystrokes for CC {:?}", cc);
+        winput::send_inputs(&inputs);
+    };
 }
